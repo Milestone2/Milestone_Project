f40fc07e239a78e1b5cc7c11092851e930204d1e
Noel Emmanuel Roodly <emmanuelroodly@yahoo.fr>
2017-08-24 11:08:54 -0400

    add BackendLess

---
+++ app/src/main/java/fr/emmanuelroodlyyahoo/milestone/Backendless.java
@@ -0,0 +1,12 @@
+package fr.emmanuelroodlyyahoo.milestone;
+
+/**
+ * Created by Emmanuel Roodly on 23/08/2017.
+ */
+
+public class Backendless {
+
+    public static final String APPLICATION_ID = "1C3FB26F-2AA1-A8D3-FFC7-DBA0F2DFBD00";
+    public static final String API_KEY = "70AB1CE1-61A5-DB9C-FF1F-095F448C2100";
+    public static final String SERVER_URL = "https://api.backendless.com";
+}
